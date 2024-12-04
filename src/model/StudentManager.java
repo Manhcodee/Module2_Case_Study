0b8401fa46c5b1211799e00cdfd1eb29fc2ba23a
Manhcodee <phammanh2952@gmail.com>
2024-12-04 16:29:31 +0700

    add student

--- src/model/StudentManager.java
+++ src/model/StudentManager.java
@@ -1,5 +1,18 @@
 package model;
 
-public class StudentManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StudentManager {
+    private List<Student> students = new ArrayList<>();
+
+    // them student
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    // xoa student
+    public void removeStudent(String studentId) {
+        students.removeIf(student -> student.getId().equals(studentId));
+    }
 }
