02d750200ec7f2bc630aface48b6c45c761e3fb0
Manhcodee <phammanh2952@gmail.com>
2024-12-05 08:50:38 +0700

    done CustomException

--- src/model/CustomException.java
+++ src/model/CustomException.java
@@ -1,4 +1,9 @@
 package model;
 
-public class CustomException {
+public class CustomException extends Exception{
+    public CustomException(String message) {
+        super(message);
+    }
 }
+
+// lop ngoai le tuy chinh de xu ly cac loi lien quan den sinh vien
